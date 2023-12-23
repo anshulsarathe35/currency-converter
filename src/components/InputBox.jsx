74fb2dbe78199507da49e372837d14695c28b109
Anshul Sarathe <anshulsarathe35@gmail.com>
2023-12-23 23:58:10 +0530

    adding files

---
+++ src/components/InputBox.jsx
@@ -0,0 +1,48 @@
+//in this component we have used useId (a hook for generating unique ids in react)
+
+import React, {useId} from 'react'
+
+function InputBox({
+    label,
+    amount,
+    onAmountChange,
+    onCurrencyChange,
+    currencyOptions = [],
+    selectCurrency = "usd",
+    amountDisable = false,
+    currencyDisable = false,
+    className = "",
+}) {
+
+const amountInputId = useId();
+
+  return (
+    <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
+        <div className='w-1/2'>
+            <label htmlFor={amountInputId} className='text-black/40 mb-2 inline-block'>{label}</label>
+            <input 
+                id={amountInputId}
+                type="number" 
+                className='outline-none w-full inline-block'
+                placeholder='Amount'
+                disabled={amountDisable}
+                value={amount}
+                onChange={(e) => onAmountChange &&
+                onAmountChange(Number(e.target.value))}/>
+        </div>
+        <div className='w-1/2 flex-wrap text-right'>
+            <p className='text-black/40 mb-2 w-full'>Currency Type</p>
+            <select className='rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none'
+            disabled={currencyDisable}
+            value={selectCurrency}
+            onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}>
+                {currencyOptions.map((currency) =>(
+                    <option key={currency} value={currency}>{currency}</option>
+                ))}
+            </select>
+        </div>
+    </div>
+  )
+}
+
+export default InputBox
